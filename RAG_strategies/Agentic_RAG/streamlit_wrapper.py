a6ee6ff20c8037d38732e4fda5531427131f3bbc
Jaeho <skyop45@naver.com>
2025-09-20 00:25:30 +0900

    Update streamlit_wrapper.py

--- RAG_strategies/Agentic_RAG/streamlit_wrapper.py
+++ RAG_strategies/Agentic_RAG/streamlit_wrapper.py
@@ -25,7 +25,7 @@ def create_graph():
     """
     # Retriever 생성 (FAISS 인덱스 사용)
     retriever = faiss.retriever(
-        index_path="./jaeho_template/RAG_strategies/Agentic_RAG/LANGCHAIN_FAISS_DB_INDEX",
+        index_path="/RAG_strategies/Agentic_RAG/LANGCHAIN_FAISS_DB_INDEX",
         fetch_k=3
     )
     
